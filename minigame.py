564f6d69299f30836a78621ac01f78089826d901
Zachary Williams <zacharywilliams080802@gmail.com>
2026-03-16 10:06:35 -0400

    deleted id_day setter in Minigame, is_day should be determined once by Checklist and never reset over the course of the Minigame

--- minigame.py
+++ minigame.py
@@ -15,13 +15,6 @@ class Minigame:
     def is_day(self) -> bool:
         return self._is_day
 
-    @is_day.setter
-    def is_day(self, is_day: bool):
-        if not isinstance(is_day, bool):
-            raise ValueError
-        else:
-            self._is_day = is_day
-
     @property
     def is_complete(self) -> bool:
         return self._is_complete
